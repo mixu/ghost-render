d00e112707af3f3330172d4eeee00fb41f665c30
Mikito Takada <mikito.takada@gmail.com>
2014-10-03 09:11:56 -0700

    Refactor pipe.js into a set of streaming functions

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-var fs = require('fs'),
-    hbs = require('express-hbs'),
-    getConfig = require('./lib/config-shim'),
-    getHelpers = require('./lib/ghost/helpers/helpers'),
-    path = require('path'),
-    miniq = require('miniq'),
-    through = require('through2'),
-    extend = require('xtend');
-
 exports.Theme = require('./lib/theme');
-exports.Pipe = require('./lib/pipe');
-
-
+exports.stream = require('./lib/stream');
